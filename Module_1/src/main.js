21993044e64092f859747b3a424edcd1b90d54ea
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-26 23:21:22 +0530

    feat: update GSAP animation for box element with fromTo method

--- Module_1/src/main.js
+++ Module_1/src/main.js
@@ -2,9 +2,16 @@ import './style.css'
 import gsap from 'gsap'
 
 
-gsap.from('.box',{
+gsap.fromTo('.box',
+  {
+    x : 0
+  },
+  
+  
+  {
   delay: 0.6,
-  x : 700,
+  x : 400,
+  y : 200,
   duration: 1,
 
 })
